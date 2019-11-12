837eb1cc8ff09d210e5e96ca9591b6776905705e
Mathis Kretz <mathis.kretz@gmail.com>
2019-11-12 22:58:33 +0100

    Add source files

---
+++ delete-all.sh
@@ -0,0 +1,1 @@
+kubectl delete deployments,services,ingresses,secrets,networkpolicies,pvc -l app=notification
---
+++ deploy-all.sh
@@ -0,0 +1,6 @@
+kubectl apply -f 00-notification-db;
+kubectl apply -f 01-notification-mq;
+kubectl apply -f 02-notification-api;
+kubectl apply -f 03-notification-mailer;
+kubectl apply -f 04-notification-ui;
+kubectl apply -f 05-network-policies;
