7482b34ee48ccb4753f296e129674d906a612eee
Tobi Fuhrimann <tobias.fuhrimann@gmail.com>
2020-09-23 00:05:19 +0200

    Clean up YAML files

--- apply-all.sh
+++ apply-all.sh
@@ -1,5 +1,7 @@
-kubectl apply -f 00-notification-db;
-kubectl apply -f 01-notification-mq;
-kubectl apply -f 02-notification-api;
-kubectl apply -f 03-notification-mailer;
-kubectl apply -f 04-notification-ui;
+#!/bin/sh
+
+kubectl apply -f 00-notification-db
+kubectl apply -f 01-notification-mq
+kubectl apply -f 02-notification-api
+kubectl apply -f 03-notification-mailer
+kubectl apply -f 04-notification-ui
